516746ca508664c645f45031029e9269bc903143
joeryvs <100026928+joeryvs@users.noreply.github.com>
2022-04-12 19:47:42 +0200

    php testen

---
+++ php_test/hello.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>more money</title>
+</head>
+
+<body>
+    <h1>extra pagina</h1>
+    <div>
+        <ol>
+            <li> <a href="index.html">return</a> </li>
+        </ol>
+    </div>
+
+    <?php 
+    
+    $hello = "mijn eigen tekst";
+
+    echo $hello;
+    /* dit is tekst die niet wordt uitgevoerd. */
+    
+    echo 777;
+    
+    ?>
+
+</body>
+
+</html>
---
+++ php_test/hello2.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+<?php 
+    $WERD = 99;
+    $zopue = "zyxwvutsrq";
+
+    echo $WERD;
+    echo $zopue;
+
+?>
+<body>
+    <h1>de titel</h1>
+</body>
+
+
+</html>
